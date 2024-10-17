b99a3f62ab8f1ffa7801aebf0d609c84327ebb97
Adetola Adedeji <adetolaadedeji0@gmail.com>
2024-10-17 09:22:17 +0100

    Practice DSA today

--- validAnagram.js
+++ validAnagram.js
@@ -2,7 +2,43 @@ let s = "anagram";
 let t = "nagaram";
 
 //check length of both strings
-//if length is same, target first value and check if it exists in the second
-//else false
-//if yes pop and move to next, continue loop
-//else false
+if (s.length === t.length) {
+  //   console.log(true);
+
+  //create dictionary from s
+  s_dict = { [s[0]]: 1 };
+
+  //   console.log(s_dict);
+  for (i = 1; i < s.length; i++) {
+    if (!(s[i] in s_dict)) {
+      s_dict[s[i]] = 1;
+    } else if (s[i] in s_dict) {
+      s_dict[s[i]] += 1;
+    }
+  }
+
+  //use t to confirm if occurences of character in s matches with t
+
+  for (j = 0; j < t.length; j++) {
+    if (t[j] in s_dict) {
+      s_dict[t[j]] -= 1;
+      if (s_dict[t[j]] < 0) {
+        console.log(false);
+      }
+    } else {
+      console.log(false);
+    }
+  }
+
+  let isAnagram = true;
+  for (let key in s_dict) {
+    if (s_dict[key] !== 0) {
+      isAnagram = false;
+      break;
+    }
+  }
+
+  console.log(isAnagram);
+} else {
+  console.log(false);
+}
