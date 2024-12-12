4e2509c280182b8106e4bd3f31a702d4f9a46118
Adetola Adedeji <adetolaadedeji0@gmail.com>
2024-12-12 08:54:15 +0100

    Complete first occurence in string problem

--- indexOfFirstOccurence.js
+++ indexOfFirstOccurence.js
@@ -1,42 +1,39 @@
 // leet  cookleetcode dbutsadandsad sad
-const haystack = "dbutsadandsan";
+const haystack = "dbutsadaxndsad";
 const needle = "adan";
 
-//the first is to check if the needle exists in the haystack
-//write for loop to determine if or where the index of the first letter to check is
-count = 1;
-let currIndex;
-let arr = [];
-let firstIndexTracker;
+//set start index  and count for the needle
+let startIndex = -1;
+let count = 0;
 
-for (i = 0; i < haystack.length; i++) {
-  //check the first letter of needle if it exists in haystack
-  if (needle[0] === haystack[i]) {
-    console.log(needle[0], i, "initialis");
-    currIndex = i; //note next index must not be greater than haystack-1
-    arr.push(i);
+//loop through the haystack
+for (let i = 0; i < haystack.length; i++) {
+  //if there is a match
+  if (haystack[i] == needle[count]) {
+    //set start index to the value of i when count is 0
+    if (count === 0) startIndex = i;
+
+    //dont forget to increment count
+    count++;
+
+    //and the loop is complete, we want to return the start index
+    if (count === needle.length) return startIndex;
   }
-  //if yes, check if the next letter exists and its index is greater than the current index in the haystack by 1
-  if (
-    currIndex + 1 < haystack.length &&
-    count < needle.length &&
-    needle[count] === haystack[currIndex + 1]
-  ) {
-    console.log(haystack[currIndex + 1], currIndex + 1);
-    currIndex += 1; //sets current index from 4 to 5
-    count += 1;
-    // i += 1;
+  //if there is a mismatch
+  else {
+    //and there was a match for a while, we want to reset the count to 0
+    //we also want i to Restart the loop from the next character after startIndex
+    //how do we check if there was a match for a while? we know, because the count will have been incremented
+    //we should also reset the startIndex to -1
+
+    if (count > 0) {
+      i = startIndex;
+      count = 0;
+      startIndex = -1;
+    }
   }
+}
 
-  // else if (needle.length - 1 === haystack[currIndex + 1]) {
-  //   console.log("done");
-  // } else if (needle[count] !== haystack[currIndex + 1]) {
-  //   //if the letter does not exist or its index is not greater than the current index
-  //   count = 1;
-  //   // i = needle[0];
-  //   console.log("arr", arr, i);
-
-  //   // console.log(i, "l");
-  //   // console.log("cc", currIndex);
-  // }
+if (count !== needle.length) {
+  return -1;
 }
